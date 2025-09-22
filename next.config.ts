96a252804ba03460fb53959df70cc5df88d475b1
Shon Dsouza <shondsouza11@gmail.com>
2025-09-23 01:15:29 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {
+      bodySizeLimit: '2mb',
+      allowedOrigins: ['*'],
+    },
   },
 }
 

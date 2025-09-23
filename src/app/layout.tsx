1b4a72a03916e140496159f084546f70696eda75
Shon Dsouza <shondsouza11@gmail.com>
2025-09-23 23:21:22 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,46 @@ import { Toaster } from 'react-hot-toast'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Tech Event Registration',
-  description: 'Register for our technical event',
+  title: 'Techspark - SDIT',
+  description: 'Register for SDIT Techspark event',
+  icons: {
+    icon: [
+      {
+        url: '/assets/logo/faviconn/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        url: '/assets/logo/faviconn/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        url: '/assets/logo/faviconn/favicon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png'
+      }
+    ],
+    apple: {
+      url: '/assets/logo/faviconn/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png'
+    },
+    shortcut: [
+      {
+        url: '/assets/logo/faviconn/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ],
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        url: '/assets/logo/faviconn/favicon.svg'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({

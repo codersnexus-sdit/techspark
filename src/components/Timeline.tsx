c1b615db8d5dd60f684d3c37d3b265f1660f581e
Shon Dsouza <shondsouza11@gmail.com>
2025-09-27 18:50:44 +0530

    initial commit

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -15,7 +15,7 @@ const TimelineSection = () => {
     {
       title: "Registration Closes",
       date: "27 September, 2025", 
-      description: "Registration will be closed on 27th",
+      description: "Registration will close on the 27th at 7 PM.",
       status: "upcoming",
       color: "text-yellow-400"
     },
